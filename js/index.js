8bd3d04c2eebe958ac530d48b6e8eca3396009b2
mnuttini <me.nuttini@gmail.com>
2016-10-23 15:30:50 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+var numerosBase=[0,1,2,3,4];
+	console.log(numerosBase)
+var variableTres=((numerosBase[2]+numerosBase[0])*12)+(Math.random()*100);
+var variableUno=++numerosBase[0];
+var variableDos=String(numerosBase[1]);
+var variableCuatro=--numerosBase[3];
+var variableCinco=variableTres%numerosBase[4]
+	console.log(variableUno);
+	console.log(variableDos);
+	console.log(variableTres);
+	console.log(variableCuatro);
+	console.log(variableCinco);
