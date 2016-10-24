98d28cd92cda15239eaabbf271364bc7ffab112f
mnuttini <me.nuttini@gmail.com>
2016-10-24 18:46:17 -0300

    tp final

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 var numerosBase=[0,1,2,3,4];
 	console.log(numerosBase)
-var variableTres=((numerosBase[2]+numerosBase[0])*12)+(Math.random()*100);
+var variableTres=((numerosBase[2]+numerosBase[0])*12)+((Math.random()*100)+1);
 var variableUno=++numerosBase[0];
-var variableDos=String(numerosBase[1]);
+var variableDos=String("javascript");
 var variableCuatro=--numerosBase[3];
 var variableCinco=variableTres%numerosBase[4]
 	console.log(variableUno);
